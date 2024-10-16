e8db611a1dddbd11f2d626ca77551691390f380d
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-10-16 22:07:32 +0530

    Finish

--- Web3_Wallet/vite.config.js
+++ Web3_Wallet/vite.config.js
@@ -4,5 +4,12 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
   plugins: [react(),nodePolyfills()],
 })
